db0969b4f234e1386f9574326205e23b9ac59b06
VUDN <dangnguyenvu.work@gmail.com>
2023-09-23 19:26:54 +0700

    feat: Change path

--- script/infer_yolov8.sh
+++ script/infer_yolov8.sh
@@ -1,3 +1,3 @@
 yolo predict \
-model=/home/vudangitwork/Documents/FTECH_Reposity/face_mask/runs/detect/train7/weights/best.pt \
-source=/home/vudangitwork/Documents/FTECH_Reposity/face_mask/data/test
+model=/home/vudangitwork/Documents/FTECH_Reposity/face_mask_detection/runs/detect/train7/weights/best.pt \
+source=/home/vudangitwork/Documents/FTECH_Reposity/face_mask_detection/data/test/images/
--- script/train_yolov8.sh
+++ script/train_yolov8.sh
@@ -1,6 +1,6 @@
 yolo train                \
 model=yolov8n.pt          \
-data=/home/vudangitwork/Documents/FTECH_Reposity/face_mask/data/custom.yaml \
+data=/home/vudangitwork/Documents/FTECH_Reposity/face_mask_detection/data/custom.yaml \
 epochs=10                 \
 imgsz=640                 \
 batch=2                   \
